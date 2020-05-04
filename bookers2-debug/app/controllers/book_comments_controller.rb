1c73e5db2e4894546ad237423088c4fdbf890b8a
vagrant <vagrant@localhost.localdomain>
2020-05-04 12:54:40 +0000

    kadai2-3

---
+++ bookers2-debug/app/controllers/book_comments_controller.rb
@@ -0,0 +1,25 @@
+class BookCommentsController < ApplicationController
+	def create
+		book = Book.find(params[:book_id])
+		book_comment = current_user.book_comments.new(book_comment_params)
+		book_comment.book_id = book.id
+		if book_comment.save
+			redirect_back(fallback_location: root_path)
+		else
+			@book = book
+			@post = Book.new
+			@book_comment = book_comment
+			render template: 'books/show'
+		end
+	end
+	def destroy
+		book = Book.find(params[:book_id])
+		book_comment = current_user.book_comments.find(params[:id])
+		book_comment.destroy
+		redirect_back(fallback_location: root_path)
+	end
+	private
+	def book_comment_params
+		params.require(:book_comment).permit(:comment)
+	end
+end
